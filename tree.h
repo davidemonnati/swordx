03cda2d1495186a4e9b4312c609167f6470e2059
Davide Monnati <davide.monnati@gmail.com>
2018-12-23 23:20:32 +0100

    cleaned code

--- tree.h
+++ tree.h
@@ -3,33 +3,9 @@
 //
 
 typedef struct Tree {
-    //char *value;
     char value[100];
     int occurrences;
     struct Tree *left;
     struct Tree *right;
 } Tree;
-
-
-
-
-/*
- * void node_print(NodePtr node_ptr, int occ) {
-    char* str = malloc(sizeof(char *));
-    if (node_ptr != NULL) {
-        node_print(node_ptr->left, occ);
-        if(occ == 0){
-            sprintf(str, "%s %i\n", node_ptr->word, node_ptr->count);
-            write_file(str);
-        }
-        else {
-            if (node_ptr->count == occ) {
-                sprintf(str, "%s %i\n", node_ptr->word, node_ptr->count);
-                write_file(str);
-            }
-        }
-        node_print(node_ptr->right, occ);
-    }
-}
-* */
 
