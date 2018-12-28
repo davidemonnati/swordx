a9c0dc5a3f7d1d100ac84e3aab7848d39cf8a9ad
Davide Monnati <davide.monnati@gmail.com>
2018-12-28 23:39:31 +0100

    Tree library

---
+++ include/tree.h
@@ -0,0 +1,15 @@
+//
+// Created by davide on 10/12/18.
+//
+
+typedef struct Tree {
+    char value[100];
+    int occurrences;
+    struct Tree *left;
+    struct Tree *right;
+} Tree;
+
+Tree *nodeAlloc();
+Tree *insertNode(Tree* albero, char *word);
+Tree *getWords(Tree* albero, char *path);
+void printTree(Tree* albero);
