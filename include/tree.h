e8163153bd2662b1d07b024635f476930b56eaba
Davide Monnati <davide.monnati@gmail.com>
2018-12-31 17:08:58 +0100

    Add toLowerCase function

--- include/tree.h
+++ include/tree.h
@@ -12,4 +12,5 @@ typedef struct Tree {
 Tree *nodeAlloc();
 Tree *insertNode(Tree* albero, char *word);
 Tree *getWords(Tree* albero, char *path);
+char *toLowerCase(char *word);
 void printTree(Tree* albero);
