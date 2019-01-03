aea9301d3fe128b7035af4f100a3edc81448734f
Davide Monnati <davide.monnati@gmail.com>
2019-01-03 18:40:55 +0100

    Moved typedef Tree implementation

--- include/tree.h
+++ include/tree.h
@@ -2,12 +2,7 @@
 // Created by davide on 10/12/18.
 //
 
-typedef struct Tree {
-    char value[100];
-    int occurrences;
-    struct Tree *left;
-    struct Tree *right;
-} Tree;
+typedef struct Tree Tree;
 
 Tree *nodeAlloc();
 Tree *insertNode(Tree* albero, char *word);
