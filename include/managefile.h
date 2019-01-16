8ecee1c14567ef0211d0a44ca910386443fba033
Davide Monnati <davide.monnati@gmail.com>
2019-01-16 14:59:27 +0100

    Update managefile.h

--- include/managefile.h
+++ include/managefile.h
@@ -1,7 +1,9 @@
-//
-// Created by davide on 29/12/18.
-//
+#include <stdio.h>
+#include <memory.h>
+#include <malloc.h>
+#include "stack.h"
 
 FILE *openFileReadMode(char *path);
 FILE *openFileWriteMode(char *path);
 void writeFile(FILE *file, char *value);
+void groupFiles(Stack *head, int argc, char **argv);
