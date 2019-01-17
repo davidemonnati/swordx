31c92a794cc8bc7fb80e27ef6f6756d6cff779f4
Davide Monnati <davide.monnati@gmail.com>
2019-01-17 19:42:34 +0100

    Revert "Deleted max_occurrency and sbo declarations"
    This reverts commit ff10cdebfe09ae089f39c4b91871a0c09f17d492.

--- include/tree.h
+++ include/tree.h
@@ -15,5 +15,7 @@ Tree *getWords(Tree* albero, char *path);
 char *toLowerCase(char *word);
 void printTree(Tree* albero);
 void printfTree(Tree* albero);
+int max_occurrency(Tree* albero, int *occ);
+void sbo(Tree* albero);
 
 #endif //TREE_MODULE_TREE_H
